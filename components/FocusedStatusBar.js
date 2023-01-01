cad35a6d59a89dc7d6fd153b296dc0037c1950db
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2023-01-01 23:57:37 +0900

    Created view layouts

--- components/FocusedStatusBar.js
+++ components/FocusedStatusBar.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'react-native';
 import { useIsFocused } from '@react-navigation/core';
 
-const FocusedStatusBar = () => {
+const FocusedStatusBar = (props) => {
     const isFocused = useIsFocused();
   
     return isFocused ? <StatusBar animated={true} {...props} /> : null;
--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { View, SafeAreaView, FlatArea, Text } from "react-native";
+import { View, SafeAreaView, FlatList, Text } from "react-native";
 
 import { COLORS, NFTData } from '../constants';
 import { NFTCard, HomeHeader, FocusedStatusBar } from "../components";
@@ -8,6 +8,31 @@ const Home = () => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <FocusedStatusBar background={COLORS.primary} />
+
+      <View style={{ flex: 1 }}>
+        <View style={{ zIndex: 0 }}>
+          <FlatList 
+            data={NFTData}
+            renderItem={({ item }) => <Text>{item.name}</Text>}
+            keyExtraction={(item) => item.id}
+            showsVerticalScrollIndicator={false}
+            ListHeaderComponent={<HomeHeader />}
+          />
+        </View>
+
+        <View style={{
+          position: "absolute",
+          top: 0,
+          bottom: 0,
+          right: 0,
+          left: 0,
+          zIndex: -1
+        }}>
+          <View style={{ height: 300, backgroundColor: COLORS.primary }} />
+          <View style={{ flex: 1, backgroundColor: COLORS.white }} />
+        </View>
+
+      </View>
     </SafeAreaView>
   )
 }
