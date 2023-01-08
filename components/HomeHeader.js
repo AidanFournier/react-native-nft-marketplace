c82b7fffdf54fa5eb58d65c52c3c9ce8799be59d
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2023-01-09 00:06:08 +0900

    Added search bar

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -1,7 +1,7 @@
 import { View, Text, Image, TextInput } from 'react-native';
 import { COLORS, FONTS, SIZES, assets } from '../constants';
 
-const HomeHeader = () => {
+const HomeHeader = ({ onSearch }) => {
   return (
     <View style={{
       backgroundColor: COLORS.primary,
@@ -28,6 +28,38 @@ const HomeHeader = () => {
             resizeMode="contain"
             style={{ position: 'absolute', width: 15, height: 15, bottom: 0, right: 0 }}
           />
+        </View>
+      </View>
+
+      <View style={{ marginVertical: SIZES.font }}>
+        <Text style={{ fontFamily: FONTS.regular, fontSize: SIZES.small, color: COLORS.white }}>
+          Hello, Andrew 👋
+        </Text>
+        <Text style={{ fontFamily: FONTS.bold, fontSize: SIZES.large, color: COLORS.white, marginTop: SIZES.base / 2}}>
+          Let's find a masterpiece
+        </Text>
+
+        <View style={{ marginTop: SIZES.font }}>
+          <View style={{
+            width: '100%',
+            borderRadius: SIZES.font,
+            backgroundColor: COLORS.gray,
+            flexDirection: 'row',
+            alignItems: 'center',
+            paddingHorizontal: SIZES.font,
+            paddingVertical: SIZES.small - 2
+          }}>
+            <Image 
+              source={assets.search}
+              resizeMode="contain"
+              style={{ width: 20, height: 20, marginRight: SIZES.base }}
+            />
+            <TextInput 
+              placeholder="Search NFTs"
+              style={{ flex: 1 }}
+              onChangeText={onSearch}
+            />
+          </View>
         </View>
       </View>
     </View>
